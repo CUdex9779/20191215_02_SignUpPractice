112799029fcf606dfb6df64c9674d77dc89d29e4
CUdex9779 <cuyu9779@gmail.com>
2019-12-15 11:54:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20191215_02_signuppractice/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a20191215_02_signuppractice
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
